d7faa1aefaed7b1774293835e1fbf1903094cd05
Jonathan Gallagher <gallagherj7@gmail.com>
2018-05-15 19:07:47 -0400

    good midpoint

--- IFA_kiosk/mail.js
+++ IFA_kiosk/mail.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({limit: '900mb', extended: true}))
 
 app.use(function (req, res) {
   res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
+ // res.write('you posted:\n')
   res.end(JSON.stringify(req.body, null, 2))
 })
 
@@ -38,6 +38,20 @@ app.use(function (req, res) {
     }
 }*/
 
+nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "photokiosk@outlook.com", // generated ethereal user
+            pass: "Elephantsredolantgrimacefear" // generated ethereal password
+        },
+        tls: {
+        ciphers:'SSLv3'
+    }
+    });
 
 
 /*app.use(express.json());       // to support JSON-encoded bodies
@@ -56,9 +70,37 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-//var received=req.body;
+var photo=req.body.image;
+var addr=req.body.address;
 
  res.send('Roastie POSTie');
+ //setTimeout(finalMail(addr),4000);
+
+  // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"bing photobooth" <photokiosk@outlook.com>', // sender address
+        to: address, // list of receivers need var
+        subject: 'Your photobooth photo', // Subject line
+        text: 'Thanks for stopping by!', // plain text body
+        html: '<b>here comes the win</b>' // html body
+    };
+
+    //base64
+    transporter.use('compile', inlineBase64({cidPrefix: 'pb_'}));
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+});
 
 });
 
@@ -107,44 +149,8 @@ app.post('/', function(req, res, next) {
 
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
-nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp-mail.outlook.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: "photokiosk@outlook.com", // generated ethereal user
-            pass: "Elephantsredolantgrimacefear" // generated ethereal password
-        },
-        tls: {
-        ciphers:'SSLv3'
-    }
-    });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"bing photobooth" <photokiosk@outlook.com>', // sender address
-        to: 'gallagherj7@gmail.com', // list of receivers need var
-        subject: 'Your photobooth photo', // Subject line
-        text: 'Thanks for stopping by!', // plain text body
-        html: '<b>Hello world?</b>' // html body
-    };
 
-    //base64
-    transporter.use('compile', inlineBase64({cidPrefix: 'pb_'}));
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-});
+   
 
